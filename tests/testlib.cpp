5a4cd87d1cd1dd4d1e9656dfd1924eb4fc323022
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-27 17:21:11 +0100

    a

--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -1,4 +1,15 @@
-#include "headers.hpp"
+
+#ifndef CATCH_TESTING_ON
+int main( int argc, char* argv[] ) {
+ 
+
+ 
+    return 0;
+}
+
+#else
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
 
 int main( int argc, char* argv[] ) {
  
@@ -7,6 +18,7 @@ int main( int argc, char* argv[] ) {
     int result = Catch::Session().run( argc, argv );
     return result;
 }
+#endif
 
 
 
