d4d101804717cebf7d4b95a4ec1d90c0faec5c24
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-10-27 09:39:13 +0200

    a

--- Byte.cpp
+++ Byte.cpp
@@ -2,13 +2,14 @@ export module Ph.Concepts.Byte;
 
 // import Ph.Concepts;
 // import Ph.Core;
+import Ph.Concepts.Core;
 import Ph.Concepts.Size;
 
 
 export 
 {
 	template <typename T, auto N>
-	concept Byte = Size <decltype (N)> and sizeof (T) <= N;
+	concept Byte = sizeof (T) <= N;
 }
 
 
