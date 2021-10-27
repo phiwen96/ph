4791e2299812d7d4e6d18f29aab2cb0ad8b780ff
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-10-27 12:27:45 +0200

    a

--- Convertible_to.cpp
+++ Convertible_to.cpp
@@ -1,9 +1,11 @@
 export module Ph.Convertible_to;
 
-import std;
 
 export template <typename T, typename U>
-concept Convertible_to = std::is_convertible_v <T, U> and (sizeof (U) >= sizeof (T));
+concept Convertible_to = (sizeof (U) >= sizeof (T)) and requires (T t, U u)
+{
+	u = t;
+};
 
 
 // module :private;
