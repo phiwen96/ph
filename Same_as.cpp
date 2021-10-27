ca3e2b27fc192241d43577a6a3eb7ec575c595e1
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-10-27 11:57:19 +0200

    a

--- Same_as.cpp
+++ Same_as.cpp
@@ -1,9 +1,22 @@
 export module Ph.Same_as;
 
-import <type_traits>;
+// import <type_traits>;
+
+template <typename T, typename U>
+struct same 
+{
+	constexpr static bool value = false;
+};
+
+template <typename T>
+struct same <T, T>
+{
+	constexpr static bool value = true;
+};
+
 
 export template <typename T, typename U>
-concept Same_as = std::is_same_v <T, U>;
+concept Same_as = same <T, U>::value;//std::is_same_v <T, U>;
 
 
 
