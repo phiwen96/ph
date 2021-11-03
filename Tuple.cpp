36c11878796bdfd50b255095a64a2328b704d328
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-11-03 14:24:15 +0100

    a

--- Tuple.cpp
+++ Tuple.cpp
@@ -10,9 +10,18 @@ namespace ph
 			true;
 		};
 
-		template <typename... T>
-		requires (sizeof... (T) > 0)
+		template <typename Head, typename... Tail>
 		struct tuple_t 
+		{
+			using self = tuple_t <Head, Tail...>;
+
+		private:
+			Head _head;
+			tuple_t <self, Tail...> _tail;
+		};
+
+		template <typename... T>
+		struct tuple_t <tuple_t <T...>>
 		{
 
 		};
@@ -22,7 +31,11 @@ namespace ph
 using namespace ph;
 
 consteval auto Tuple_test () noexcept -> bool 
+
 {
+	static_assert (Tuple <tuple_t <int>>);
+
+	
 
 	return true;
 }
---
+++ clutter.cpp
@@ -0,0 +1,17 @@
+
+
+
+requires requires (tuple_t <int, char> t)
+{
+	requires 2+3 == 5;
+}
+
+
+only check if syntax valid
+requires requires ()
+{
+	requires ()
+	{
+		2 == 3;
+	};
+}
