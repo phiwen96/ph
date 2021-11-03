2824655a30cf3f434f785c51606756598575cd3f
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-11-03 12:58:31 +0100

    a

---
+++ Concepts.cpp
@@ -0,0 +1,35 @@
+export module Ph.Concepts;
+
+export import Ph.Concepts.Bool;
+export import Ph.Concepts.Done;
+export import Ph.Concepts.Size;
+export import Ph.Concepts.Bit;
+export import Ph.Concepts.Byte;
+export import Ph.Concepts.Core;
+export import Ph.Concepts.Char;
+export import Ph.Concepts.String;
+export import Ph.Concepts.Pointer;
+export import Ph.Concepts.Number;
+export import Ph.Concepts.Error;
+export import Ph.Concepts.Void;
+
+export import Ph.Concepts.Iterator;
+export import Ph.Concepts.Array;
+
+export import Ph.Concepts.Class;
+export import Ph.Concepts.Enum;
+export import Ph.Concepts.Function;
+export import Ph.Concepts.Any_of;
+export import Ph.Concepts.File;
+export import Ph.Concepts.Vector;
+export import Ph.Concepts.Common;
+export import Ph.Concepts.Constant;
+export import Ph.Concepts.Range;
+export import Ph.Concepts.Element;
+export import Ph.Concepts.Types;
+
+
+namespace ph 
+{
+
+}
