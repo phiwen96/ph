48637337d1896e136f63c7324ef46d39f6f4ee20
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-11-03 13:29:04 +0100

    a

--- Strings.cpp
+++ Strings.cpp
@@ -13,17 +13,20 @@ namespace ph
 		template <typename S>
 		concept Strings = Range <S> and requires (S s) 
 		{
-			{s [0]} -> Char;
+			{s [0]} -> String;
 			{s [0]} -> Reference;
 		};
 	}
 }
 
 using namespace ph;
+import std; 
 
 consteval auto Strings_test () -> bool 
 {
-
+	static_assert (Strings <std::vector <std::string>>);
+	static_assert (Strings <std::array <std::string, 10>>);
+	static_assert (Strings <std::vector <char const*>>);
 	return true;
 }
 
