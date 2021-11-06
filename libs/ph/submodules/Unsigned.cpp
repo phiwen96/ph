971fef40a9716e7305f0dd9ebf07f15da2b241f9
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-11-06 13:22:12 +0100

    a

--- libs/ph/submodules/Unsigned.cpp
+++ libs/ph/submodules/Unsigned.cpp
@@ -6,16 +6,27 @@ import Ph.Assert;
 
 namespace ph 
 {
-
-export template <typename T>
-concept Unsigned = Convertible_to <T, unsigned short>
-    or Convertible_to <T, unsigned short int>
-    or Convertible_to <T, unsigned>
-    or Convertible_to <T, unsigned int>
-    or Convertible_to <T, unsigned long>
-    or Convertible_to <T, unsigned long int>
-    or Convertible_to <T, unsigned long long>
-    or Convertible_to <T, unsigned long long int>;
-
+	export template <typename T>
+	concept Unsigned = Convertible_to <T, unsigned short>
+		or Convertible_to <T, unsigned short int>
+		or Convertible_to <T, unsigned>
+		or Convertible_to <T, unsigned int>
+		or Convertible_to <T, unsigned long>
+		or Convertible_to <T, unsigned long int>
+		or Convertible_to <T, unsigned long long>
+		or Convertible_to <T, unsigned long long int>;
 }
+
+
+consteval auto Unsigned_test () -> bool
+{
+	using namespace ph;
+
+	constexpr auto assert_unsigned = [] <Unsigned> {};
+	constexpr auto assert_not_unsigned = [] <typename T> requires (not Unsigned <T>) {};
+	
+	return true;
+}
+
+static_assert (Unsigned_test ());
 
