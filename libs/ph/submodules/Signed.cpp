b427bed7ed44586eb04cb26c487df121f1111219
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-11-06 13:21:19 +0100

    a

--- libs/ph/submodules/Signed.cpp
+++ libs/ph/submodules/Signed.cpp
@@ -6,25 +6,35 @@ import Ph.Assert;
 
 namespace ph 
 {
+	export 
+	{
+		template <typename T>
+		concept Signed = Convertible_to <T, short>
+			or Convertible_to <T, short int>
+			or Convertible_to <T, signed short>
+			or Convertible_to <T, signed short int>
+			or Convertible_to <T, int>
+			or Convertible_to <T, signed>
+			or Convertible_to <T, signed int>
+			or Convertible_to <T, long>
+			or Convertible_to <T, long int>
+			or Convertible_to <T, signed long>
+			or Convertible_to <T, signed long int>
+			or Convertible_to <T, long long>
+			or Convertible_to <T, long long int>
+			or Convertible_to <T, signed long long>
+			or Convertible_to <T, signed long long int>;
+	}
+}
 
-export 
+consteval auto Signed_test () -> bool
 {
-	template <typename T>
-	concept Signed = Convertible_to <T, short>
-		or Convertible_to <T, short int>
-		or Convertible_to <T, signed short>
-		or Convertible_to <T, signed short int>
-		or Convertible_to <T, int>
-		or Convertible_to <T, signed>
-		or Convertible_to <T, signed int>
-		or Convertible_to <T, long>
-		or Convertible_to <T, long int>
-		or Convertible_to <T, signed long>
-		or Convertible_to <T, signed long int>
-		or Convertible_to <T, long long>
-		or Convertible_to <T, long long int>
-		or Convertible_to <T, signed long long>
-		or Convertible_to <T, signed long long int>;
+	using namespace ph;
+
+	constexpr auto assert_signed = [] <Signed> {};
+	constexpr auto assert_not_signed = [] <typename T> requires (not Signed <T>) {};
+	
+	return true;
 }
 
-}
+static_assert (Signed_test ());
